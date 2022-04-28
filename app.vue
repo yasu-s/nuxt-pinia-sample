7ff67974399b6921a502d94468e66a15db44ad5a
Yasuyuki Saito <ya_saito@uluru.jp>
2022-04-28 16:38:32 +0900

    store追加

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
+<script lang="ts" setup>
+import { useCounterStore } from '@/store/counter'
+
+/** store */
+const counterStore = useCounterStore()
+
+/**
+ * increment実行
+ */
+function increment() {
+  counterStore.increment()
+}
+
+/**
+ * storeの状態をリセット
+ */
+function reset() {
+  counterStore.$reset()
+}
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <div>
+      <button @click="increment">increment</button>
+      <button @click="reset">reset</button>
+    </div>
+    <div>count: {{ counterStore.count }}</div>
+    <div>double: {{ counterStore.double }}</div>
   </div>
 </template>
